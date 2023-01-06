4650f88cbbf58b03dc104c43458358ab392de9b2
rinban <rinad.k.bankher@gmail.com>
2023-01-07 00:58:11 +0300

    build server side

--- server.js
+++ server.js
@@ -4,16 +4,46 @@ projectData = {};
 // Require Express to run server and routes
 const express = require('express');
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+
+// port for listening
+const port = 8000;
+// create express server using the listen method
+const server = app.listen(port,listening);
+
+// will show on terminal
+function listening(){
+    console.log('server is running');
+    console.log(`running on localhost: ${port}`);
+}
+
+// GET server route
+app.get('/allData',function(req,res){
+    res.send(projectData);
+});
+
+// Post server route
+app.post('/addData', postData)
+
+function postData (req,res){
+
+    projectData = req.body;
+    res.send(projectData);
+}
